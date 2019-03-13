0ad53f5a132cec62841ba67699aadf1b4b0676b9
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2019-03-13 16:41:13 -0400

    Create small task Access todos object and render it as a prop

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Start washer',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Wash dishes',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Walk Snooky',
+        completed: false
+      }
+    ]
+  }
   render() {
+    // console.log(this.state.todos)
     return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
